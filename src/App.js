300635bad3382229b42f7efff8c46cbdd7e65c0d
Yves Faems <yves.faems@gmail.com>
2022-04-22 13:25:00 -0700

    title and weather sections

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import './App.css';
+import styles from "./App.module.css";
 
 function App() {
-  return (
-    <div className="App">
-        <h3>5 day weather forecast LOS ANGELES</h3>
-
-    </div>
-  );
+	return (
+		<div className={styles.app}>
+			<section>
+				<h2>5 day weather forecast</h2>
+				<br />
+				<h1>Los Angeles</h1>
+			</section>
+            <section>
+                weather data
+            </section>
+		</div>
+	);
 }
 
 export default App;
